56af1bed58b2d578637723fe22d382a758fdd03e
Alexander <mr.vasilev9810@yandex.ru>
2023-09-18 20:53:54 +0300

    server fix

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,16 +3,20 @@ import { TrackModule } from './track/track.module';
 import {MongooseModule} from "@nestjs/mongoose";
 import { FileModule } from './file/file.module';
 import {ServeStaticModule} from "@nestjs/serve-static";
+import { ConfigModule } from '@nestjs/config';
 import * as path from 'path'
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static'),
     }),
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.8vephp0.mongodb.net/spotify-clone?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(process.env.DB_URI),
     TrackModule,
     FileModule
   ]
